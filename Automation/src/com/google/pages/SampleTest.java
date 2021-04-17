98448ea1ed22ec3ae6640340cc54e539010b45ac
User <User@192.168.1.6>
2021-04-17 11:33:22 +0530

    commiting to framework folder git

---
+++ Automation/src/com/google/pages/SampleTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.google.pages;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author User
+ *
+ */
+public class SampleTest {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("hi");
+	}
+
+}
